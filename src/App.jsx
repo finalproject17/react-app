f5454b3bc2925497c53cfe2099f3e370b9490c55
Your Name <safynazabdelraheem@gmail.com>
2024-05-26 17:48:57 +0300

    editing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Navbar from './component/Navbar/index';
-import Home from '../src/pages/JobSeeker/index'
+// import Home from '../src/pages/JobSeeker/index'
+import JobSeeker from '../src/pages/JobSeeker';
 
 function App() {
     return (
@@ -11,7 +12,7 @@ function App() {
             {/* <Navbar /> */}
             <Routes>
                 {/* Define your routes here */}
-                <Route path="/JobSeeker" element={<Home />} />
+                <Route path="/JobSeeker" element={<JobSeeker />} />
                 {/* <Route path="/find-jobs" element={<FindJobs />} />
                 <Route path="/about-us" element={<AboutUs />} />
                 <Route path="/contact" element={<Contact />} />
