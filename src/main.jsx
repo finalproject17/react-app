ac6f7afbacaa6d2d6962b1756f92b811a326926a
shroukgamall17 <shrouqgamal17@gmail.com>
2024-05-26 14:44:18 +0300

    merging Madonna

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,16 @@
-import React from 'react'
+//import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
-=======
->>>>>>> ca4f8ff9d106ff93e8d3cdca3bf03da361da06e2
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   <BrowserRouter>
-=======
-  
->>>>>>> 87886ffb7d38ccf703e10248f2af0dda37b3862d
+
     <App />
     </BrowserRouter>
-=======
-    <App />
->>>>>>> ca4f8ff9d106ff93e8d3cdca3bf03da361da06e2
+
   // </React.StrictMode>
 )
