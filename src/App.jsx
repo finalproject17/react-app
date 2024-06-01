e26e9e70df8792e81b08e1b0976a7d429897a1e7
mary7895 <maryharby540@gmail.com>
2024-06-01 17:47:04 +0300

    Mary

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import JobSeekerCard from './component/JobSeekerCard';
+import JobCard from './component/JobCard';
+import EduCard from './component/EduCard';
 
 
 function App() {
   
 
   return (
-    < >
-    <JobSeekerCard></JobSeekerCard>
-   </>
-  )
+    <>
+      <JobSeekerCard></JobSeekerCard>
+      <JobCard></JobCard>
+      <EduCard></EduCard>
+    </>
+  );
 }
 
 export default App
