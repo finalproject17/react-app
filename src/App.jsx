10e542dc685b193642f40a82da37ae682308d597
kholoud hanafy <kholuodhanafy3@gmail.com>
2024-06-08 21:18:16 +0300

    add jobDetails Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,55 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import JobSeeker from "./pages/JobSeeker";
-//import  JobsDetails from './pages/JobsDetails'
-
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import  JobSeeker from './pages/JobSeeker'
-
 import Login from "./component/login";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Jobs from "../src/pages/Jobs";
 import JobSeekerProfile from "./pages/JobSeekerProfile";
+import Footer from "./component/Footer/index";
+import Navbar from "./component/Navbar/index";
+import JobInfoCard from "./component/JobInfoCard/index"
 import "./App.css";
-import JobSeekerCard from "./component/JobSeekerCard";
 import JobCard from "./component/JobCard";
 import EduCard from "./component/EduCard";
-
+import JobCategories from "./component/JobCategories/JobCategories";
+import JobSeekerProfileCard from "./component/jobSeekerProfileCard";
+import JobsDetails from "./pages/JobsDetails";
 function App() {
+  const jobInfo = {
+    img: "Edit.svg", // Replace with your image path
+    text: "UI UX",
+    backgroundColor: "#f0f0f0" // Optional: replace with desired background color
+  };
   return (
     <>
 
      
-   <JobSeeker></JobSeeker>
-   
-
-
+   <Navbar/> 
+     <JobsDetails/>
+     
+    <Footer/>
+   {/* <JobInfoCard
+        img={jobInfo.img} 
+        text={jobInfo.text} 
+        backgroundColor={jobInfo.backgroundColor} 
+      />
+  
+  <JobCard/>
+  <EduCard/>
+  <JobSeekerProfileCard/> */}
   
 
-      <BrowserRouter>
-        <Routes>
+
+      
+        {/* <Routes>
           <Route path="/" element={<JobSeeker />} />
           <Route path="/Jobs" element={<Jobs />} />
           <Route path="/profile" element={<JobSeekerProfile />} />
           <Route path="/login" element={<Login />} />
         </Routes>
-      </BrowserRouter>
+     */}
     </>
   );
 }
